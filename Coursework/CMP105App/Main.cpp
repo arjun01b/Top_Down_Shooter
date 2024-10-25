d03f71da24337855fd332b784fb78f542699ad67
arjun01bhatnagar <146191169+arjun01b@users.noreply.github.com>
2024-10-25 14:03:55 +0530

    Details

--- Coursework/CMP105App/Main.cpp
+++ Coursework/CMP105App/Main.cpp
@@ -71,7 +71,7 @@ void windowProcess(sf::RenderWindow* window, Input* in)
 int main()
 {
 	//Create the window
-	sf::RenderWindow window(sf::VideoMode(1200, 675), "CMP105_Coursework");
+	sf::RenderWindow window(sf::VideoMode(1200, 675), "TopDownShooter");
 
 	// Initialise input and level objects.
 	AudioManager audioManager;
